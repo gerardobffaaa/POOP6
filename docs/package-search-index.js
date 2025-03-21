e08a0236e889d46e207d6bfddc873105444442a7
gerardobffaaa <gerardobffaaa@hotmail.com>
2025-03-20 21:38:17 -0600

    Add files via upload

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mx.UNAM.FI.POO"},{"l":"pokemon"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"pokemon","l":"Pokemon"},{"p":"mx.UNAM.FI.POO","l":"POOP06"}];updateSearchResults();
